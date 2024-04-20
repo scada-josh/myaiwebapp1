16eea3da18ac6ee3260b0d34b6495e357f10e594
scada-josh <scada.josh@gmail.com>
2024-04-20 16:34:54 +0700

    3rd

--- myaiwebapp1.py
+++ myaiwebapp1.py
@@ -4,7 +4,7 @@ import google.generativeai as genai
 genai.configure(api_key=st.secrets["genai_key"])
 model = genai.GenerativeModel("gemini-pro")
 
-st.title("แปลภาษา")
+st.title("แปลภาษา จ๊ะ")
 ch = st.selectbox("เลือกภาษาปลายทาง",
                  ("ไทย","อังกฤษ","เกาหลี","ญี่ปุ่น"))
 
